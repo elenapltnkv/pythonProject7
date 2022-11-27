6c6c4763254ac6e36c34106fb054f6ae84069aae
Ruslan Samigullaev <samigullaev@inbox.ru>
2022-11-27 16:58:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+name = input('your name: ')
+print('Hello, ', name)
+parol = 'ddd333'
+inparol = input('input your password:')
+if parol == inparol:
+    print('welcome!')
+else:
+    print('wrong password!')
